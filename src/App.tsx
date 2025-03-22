ac860d7734467f7d2ca7bd14e167d551add7d150
lssb2003 <lssb2003@gmail.com>
2025-03-22 22:43:30 +0800

    2025

--- src/App.tsx
+++ src/App.tsx
@@ -487,7 +487,7 @@ const AppContent: React.FC = () => {
       </main>
 
       <footer className="app-footer">
-        <p>C3: Context-Aware Code Companion &copy; 2023</p>
+        <p>C3: Context-Aware Code Companion &copy; 2025</p>
       </footer>
     </div>
   );
